6ff61e7eb5786ba7edc241e74915e9235c8ba54f
Wpaswan <dablumcapaswan@gmail.com>
2021-12-17 09:36:20 -0800

    [I added a code to search for a node]

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -3,4 +3,18 @@ Console.WriteLine("************Welcome to BST Program********"); //[3,7,9,11,55,
 BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
 binarySearch.Insert(30);
 binarySearch.Insert(70);
+binarySearch.Insert(22);
+binarySearch.Insert(40);
+binarySearch.Insert(60);
+binarySearch.Insert(95);
+binarySearch.Insert(11);
+binarySearch.Insert(65);
+binarySearch.Insert(3);
+binarySearch.Insert(16);
+binarySearch.Insert(63);
+binarySearch.Insert(67);
+binarySearch.GetSize();
+bool result = binarySearch.IfExists(67, binarySearch);
+Console.WriteLine(result);
+Console.ReadKey();
 binarySearch.Display();
