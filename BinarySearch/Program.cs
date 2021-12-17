aeb7cb72757013cba2678df7f067f1402c584f78
Wpaswan <dablumcapaswan@gmail.com>
2021-12-17 09:21:23 -0800

    [I added a code to create Binary search tree]

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,6 @@
+﻿using BinarySearch;
+Console.WriteLine("************Welcome to BST Program********"); //[3,7,9,11,55,67]
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Display();
